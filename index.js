704b607cdca7b965e8c7355ac1dd13ee076e398f
vaibhav <vaibhavkedia24@gmail.com>
2022-01-23 23:48:40 +0530

    Organization route

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+require("./helpers/dbconnect");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const routes = require("./routes/index");
+
+//Express Parser
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+//Routes
+app.use("/", routes);
+
+// Home route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome, This is App's Backend" });
+});
+
+//Server Start
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ routes/organizations/index.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const Schemas = require("../../models/index");
+const mongoose = require("mongoose");
+const getOrganizations = require("./getOrganizations");
+const addOrganizations = require("./addOrganizations");
+const updateOrganizations = require("./updateOrganizations");
+const deleteOrganizations = require("./deleteOrganizations");
+
+//Get Organization
+router.get("/", getOrganizations);
+
+//Add Organization
+router.post("/", addOrganizations);
+
+//Update Organization
+//router.post("/update" , updateOrganizations)
+
+//Delete Organization
+router.delete("/", deleteOrganizations);
+
+module.exports = router;
